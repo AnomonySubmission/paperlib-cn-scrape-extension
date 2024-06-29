228170c3c70431df4f8e727b42b0a9ff91245d96
AnomonySubmission <Anomony.Submission@gmail.com>
2024-06-29 22:44:22 +0100

    fix: cnki

--- src/scrapers/cnki.ts
+++ src/scrapers/cnki.ts
@@ -1,9 +1,10 @@
 import parse from "node-html-parser";
 import { PaperEntity } from "paperlib-api/model";
-import { PLExtAPI } from "paperlib-api/api";
+import { PLAPI, PLExtAPI } from "paperlib-api/api";
 
 import { isEmpty } from "@/utils/string";
 import { Scraper, ScraperRequestType } from "./scraper";
+import { stringUtils } from "paperlib-api/utils";
 
 export class CNKIScraper extends Scraper {
   static checkEnable(paperEntityDraft: PaperEntity): boolean {
@@ -11,7 +12,7 @@ export class CNKIScraper extends Scraper {
   }
 
   static preProcess(paperEntityDraft: PaperEntity): ScraperRequestType {
-    const scrapeURL = `https://kns.cnki.net/kns/brief/GetGridTableHtml`;
+    const scrapeURL = `https://kns.cnki.net/kns/brief/grid`;
 
     const headers = {
       Accept: "text/html, */*; q=0.01",
@@ -22,7 +23,6 @@ export class CNKIScraper extends Scraper {
       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
       Host: "kns.cnki.net",
       Origin: "https://kns.cnki.net",
-      Pragma: "no-cache",
       Referer: "https://kns.cnki.net/kns/defaultresult/index",
       "Sec-Fetch-Dest": "empty",
       "Sec-Fetch-Mode": "cors",
@@ -42,20 +42,40 @@ export class CNKIScraper extends Scraper {
   static parsingProcess(rawResponse: string): PaperEntity[] {
     const html = parse(rawResponse);
 
-    const results = html.querySelectorAll("tr")
+    const results = html.querySelectorAll("tr");
 
-    const candidatePaperEntityDrafts: PaperEntity[] = []
+    const candidatePaperEntityDrafts: PaperEntity[] = [];
 
     for (const result of results) {
-      let title = result.querySelector("td:nth-child(2)")?.querySelector("a")?.innerText.trim();
+      let title = result
+        .querySelector("td:nth-child(2)")
+        ?.querySelector("a")
+        ?.innerText.trim();
+
       if (!title) continue;
-      const candidatePaperEntityDraft = new PaperEntity()
+      const candidatePaperEntityDraft = new PaperEntity();
       candidatePaperEntityDraft.title = title;
-      candidatePaperEntityDraft.authors = result.querySelector("td:nth-child(3)")?.innerText.trim().split(";").map((author) => author.trim()).filter(v => v).join(", ") || "";
-      candidatePaperEntityDraft.publication = result.querySelector("td:nth-child(4)")?.innerText.trim() || "";
-      candidatePaperEntityDraft.pubTime = result.querySelector("td:nth-child(5)")?.innerText.trim().substring(0, 4) || "";
+      candidatePaperEntityDraft.authors =
+        result
+          .querySelector("td:nth-child(3)")
+          ?.innerText.trim()
+          .split(";")
+          .map((v) => stringUtils.formatString({ str: v, removeNewline: true }))
+          .map((v) => v.split(" ").filter((v) => v))
+          .flat()
+          .map((author) => author.trim())
+          .filter((v) => v)
+          .join(", ") || "";
+      candidatePaperEntityDraft.publication =
+        result.querySelector("td:nth-child(4)")?.innerText.trim() || "";
+      candidatePaperEntityDraft.pubTime =
+        result
+          .querySelector("td:nth-child(5)")
+          ?.innerText.trim()
+          .substring(0, 4) || "";
       const typeStr = result.querySelector("td:nth-child(6)")?.innerText.trim();
-      candidatePaperEntityDraft.pubType = typeStr === "期刊" ? 0 : typeStr === "会议" ? 1 : 2;
+      candidatePaperEntityDraft.pubType =
+        typeStr === "期刊" ? 0 : typeStr === "会议" ? 1 : 2;
 
       candidatePaperEntityDrafts.push(candidatePaperEntityDraft);
     }
@@ -74,22 +94,90 @@ export class CNKIScraper extends Scraper {
     const { scrapeURL, headers, sim_threshold } =
       this.preProcess(paperEntityDraft);
 
-    const queryJson = `{"Platform":"","DBCode":"CFLS","KuaKuCode":"CJFQ,CDMD,CIPD,CCND,CYFD,CCJD,BDZK,CISD,CJFQ,CDMD,CIPD,CCND,CYFD,CCJD,BDZK,CISD,CJFN","QNode":{"QGroup":[{"Key":"Subject","Title":"","Logic":1,"Items":[{"Title":"主题","Name":"SU","Value":"${paperEntityDraft.title} ${paperEntityDraft.authors.split(",").shift()?.trim() || ""}","Operate":"%=","BlurType":""}],"ChildItems":[]}]},"CodeLang":"ch"}`
+    // From Zotero jasminum
+    const queryJson: any = {
+      Platform: "",
+      Resource: "CROSSDB",
+      DBCode: "SCDB",
+      KuaKuCode: "CJZK,CDFD,CMFD,CPFD,IPFD,CCND,BDZK,CPVD",
+      QNode: {
+        QGroup: [
+          {
+            Key: "Subject",
+            Title: "",
+            Logic: 0,
+            Items: [],
+            ChildItems: [], // fill up here
+          },
+          {
+            Key: "ControlGroup",
+            Title: "",
+            Logic: 0,
+            Items: [],
+            ChildItems: [],
+          },
+        ],
+      },
+      ExScope: "1",
+      SearchType: "0",
+    };
+    if (paperEntityDraft.authors) {
+      const au = {
+        Key: "",
+        Title: "",
+        Logic: 0,
+        Items: [
+          {
+            Key: "",
+            Title: "作者",
+            Logic: 0,
+            Field: "AU",
+            Operator: "DEFAULT",
+            Value: paperEntityDraft.authors,
+            Value2: "",
+          },
+        ],
+        ChildItems: [],
+      };
+
+      queryJson.QNode.QGroup[0].ChildItems.push(au);
+    }
+    const su = {
+      Key: "",
+      Title: "",
+      Logic: 0,
+      Items: [
+        {
+          Key: "",
+          Title: "主题",
+          Logic: 0,
+          Field: "SU",
+          Operator: "TOPRANK",
+          Value: stringUtils.formatString({
+            str: paperEntityDraft.title,
+            removeSymbol: true,
+          }),
+          Value2: "",
+        },
+      ],
+      ChildItems: [],
+    };
+    queryJson.QNode.QGroup[0].ChildItems.push(su);
 
     const data = {
-      IsSearch: true,
-      QueryJson: queryJson,
-      PageName: "defaultresult",
       DBCode: "SCDB",
-      KuaKuCodes:
-        "CJFQ,CDMD,CIPD,CCND,CYFD,CCJD,BDZK,CISD,CJFQ,CDMD,CIPD,CCND,CYFD,CCJD,BDZK,CISD,CJFN",
-      CurPage: 1,
-      RecordsPerPage: 20,
+      pageNum: "1",
+      pageSize: "20",
+      sortField: "PT",
+      sortType: "desc",
+      boolSearch: "true",
+      boolSortSearch: "false",
+      version: "kns7",
       CurDisplayMode: "listmode",
-      CurrSortField: "",
-      CurrSortFieldType: "desc",
-      IsSentenceSearch: false,
-      Subject: "",
+      productStr: "CJZK,CDFD,CMFD,CPFD,IPFD,CCND,BDZK,CPVD",
+      sentenceSearch: "false",
+      aside: "空",
+      QueryJson: JSON.stringify(queryJson),
     };
 
     const formBody: string[] = [];
@@ -100,13 +188,16 @@ export class CNKIScraper extends Scraper {
     }
     const formBodyStr = formBody.join("&");
 
+    PLAPI.logService.info(scrapeURL, "", false, "CNKIScraper");
+    PLAPI.logService.info(formBodyStr, "", false, "CNKIScraper");
+
     const response = await PLExtAPI.networkTool.post(
       scrapeURL,
       formBodyStr,
       headers,
       1,
       10000,
-      false,
+      false
     );
 
     const candidatePaperEntityDrafts = this.parsingProcess(response.body);
